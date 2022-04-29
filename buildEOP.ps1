2a3ed22fd5ed6e5e84bf089b416e8f517a50c04b
youneuoy <61095686+youneuoy@users.noreply.github.com>
2022-04-29 19:15:24 +0300

    script updating

--- buildEOP.ps1
+++ buildEOP.ps1
@@ -10,14 +10,14 @@ Remove-item ./logs -recurse -erroraction 'silentlycontinue' | Out-Null
 new-item ./logs -itemtype directory -erroraction 'silentlycontinue' | Out-Null
 
 Write-Host "build main project"
-devenv  "..\M2TWEOP-library\M2TWEOP library.sln" /build Release /project "M2TWEOP library" /out "logs\b.log"| Out-Null
-devenv  "..\M2TWEOP-library\M2TWEOP library.sln" /build Release /project "M2TWEOP GUI" /out "logs\GUI.log" | Out-Null
-devenv  "..\M2TWEOP-library\M2TWEOP library.sln" /build Release /project "M2TWEOP tools"  /out "logs\tools.log" | Out-Null
-devenv  "..\M2TWEOP-library\M2TWEOP library.sln" /build Release /project "d3d9"  /out "logs\d3d9.log" | Out-Null
+devenv  "M2TWEOP-library\M2TWEOP library.sln" /build "Release|x86" /project "M2TWEOP library" /out "logs\library.log"| Out-Null
+devenv  "M2TWEOP-library\M2TWEOP library.sln" /build "Release|x86" /project "M2TWEOP GUI" /out "logs\GUI.log" | Out-Null
+devenv  "M2TWEOP-library\M2TWEOP library.sln" /build "Release|x86" /project "M2TWEOP tools"  /out "logs\tools.log" | Out-Null
+devenv  "M2TWEOP-library\M2TWEOP library.sln" /build "Release|x86" /project "d3d9"  /out "logs\d3d9.log" | Out-Null
 
 Write-Host "build lua plugin"
 #build plugin
-devenv  "..\M2TWEOP-luaPlugin\luaPlugin.sln" /build Release /project "luaPlugin"  /out "logs\luaPlugin.log" | Out-Null
+devenv  "M2TWEOP-luaPlugin\luaPlugin.sln" /build "Release|x86" /project "luaPlugin"  /out "logs\luaPlugin.log" | Out-Null
 
 Write-Host "Make documentation"
 cd "documentationGenerator"
@@ -34,13 +34,13 @@ new-item ./M2TWEOPGenerated  -itemtype directory -erroraction 'silentlycontinue'
 
 
 Copy-Item -Path  "M2TWEOP-DataFiles\*" -Destination "./M2TWEOPGenerated" -recurse 
-Copy-Item -Path  "..\M2TWEOP-luaPlugin\docs\EOPDocs\build\html\*" -Destination "./M2TWEOPGenerated/eopData/helpPages" -recurse 
+Copy-Item -Path  "documentationGenerator\EOPDocs\build\html\*" -Destination "./M2TWEOPGenerated/eopData/helpPages" -recurse 
 
-Copy-Item -Path  "..\M2TWEOP-luaPlugin\Release\luaPlugin.dll" -Destination "./M2TWEOPGenerated/youneuoy_Data/plugins"
-Copy-Item -Path  "..\M2TWEOP-library\Release\d3d9.dll" -Destination "./M2TWEOPGenerated"
-Copy-Item -Path  "..\M2TWEOP-library\Release\M2TWEOP GUI.exe" -Destination "./M2TWEOPGenerated" 
-Copy-Item -Path  "..\M2TWEOP-library\Release\M2TWEOP tools.exe" -Destination "./M2TWEOPGenerated" 
-Copy-Item -Path  "..\M2TWEOP-library\Release\M2TWEOPLibrary.dll" -Destination "./M2TWEOPGenerated" 
+Copy-Item -Path  "M2TWEOP-luaPlugin\Release\luaPlugin.dll" -Destination "./M2TWEOPGenerated/youneuoy_Data/plugins"
+Copy-Item -Path  "M2TWEOP-library\Release\d3d9.dll" -Destination "./M2TWEOPGenerated"
+Copy-Item -Path  "M2TWEOP-library\Release\M2TWEOP GUI.exe" -Destination "./M2TWEOPGenerated" 
+Copy-Item -Path  "M2TWEOP-library\Release\M2TWEOP tools.exe" -Destination "./M2TWEOPGenerated" 
+Copy-Item -Path  "M2TWEOP-library\Release\M2TWEOPLibrary.dll" -Destination "./M2TWEOPGenerated" 
 
 Remove-item M2TWEOP.zip -erroraction 'silentlycontinue' | Out-Null
 Compress-Archive -Path "./M2TWEOPGenerated/*"  -DestinationPath "M2TWEOP.zip"
