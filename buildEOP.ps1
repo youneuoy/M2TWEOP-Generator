6f77a80fb6a438371418df56bf519b9d2d28aedd
Eddie Eldridge <steadyeddie101@hotmail.co.uk>
2022-05-02 11:43:28 +0100

    Update script

--- buildEOP.ps1
+++ buildEOP.ps1
@@ -2,7 +2,7 @@
 #Microsoft Visual Studio 2019 in its default folder
 $currentLoc=(get-location).path
 Write-Host "importing visual studio module"
-&{Import-Module "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"; Enter-VsDevShell 06308686}
+&{Import-Module "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"; Enter-VsDevShell b3468e38}
 
 Set-Location -Path $currentLoc
 
