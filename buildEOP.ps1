41f8565d015a0d6bce5ab687942db1016884996c
Eddie Eldridge <eddie.eldridge@genesys.com>
2022-05-06 12:33:49 +0100

    Update output

--- buildEOP.ps1
+++ buildEOP.ps1
@@ -1,53 +1,59 @@
 #requiments:
 #Microsoft Visual Studio 2019 in its default folder
 $currentLoc=(get-location).path
-Write-Host "importing visual studio module"
+Write-Host "======== 0) Pre Cleanup ========\n" -ForegroundColor Yellow
 # &{Import-Module "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll";}
-
 # $vsPath = &(Join-Path ${env:ProgramFiles(x86)} "\Microsoft Visual Studio\Installer\vswhere.exe") -property installationpath
 # Import-Module (Join-Path $vsPath "Common7\Tools\Microsoft.VisualStudio.DevShell.dll")
 # Enter-VsDevShell -VsInstallPath $vsPath -SkipAutomaticLocation
 
 Set-Location -Path $currentLoc
+Remove-item ./logs -recurse -erroraction 'silentlycontinue'
+new-item ./logs -itemtype directory -erroraction 'silentlycontinue'
 
-Remove-item ./logs -recurse -erroraction 'silentlycontinue' 
-new-item ./logs -itemtype directory -erroraction 'silentlycontinue' 
+# 1) Build M2TWEOP-library
+Write-Host "======== 1) Build M2TWEOP-library ========\n" -ForegroundColor Yellow
 
-Write-Host "build main project"
 devenv  "M2TWEOP-library\M2TWEOP library.sln" /build "Release|x86" /project "M2TWEOP library" /out "logs\library.log"
-devenv  "M2TWEOP-library\M2TWEOP library.sln" /build "Release|x86" /project "M2TWEOP GUI" /out "logs\GUI.log" 
-devenv  "M2TWEOP-library\M2TWEOP library.sln" /build "Release|x86" /project "M2TWEOP tools"  /out "logs\tools.log" 
-devenv  "M2TWEOP-library\M2TWEOP library.sln" /build "Release|x86" /project "d3d9"  /out "logs\d3d9.log" 
+devenv  "M2TWEOP-library\M2TWEOP library.sln" /build "Release|x86" /project "M2TWEOP GUI" /out "logs\GUI.log"
+devenv  "M2TWEOP-library\M2TWEOP library.sln" /build "Release|x86" /project "M2TWEOP tools"  /out "logs\tools.log"
+devenv  "M2TWEOP-library\M2TWEOP library.sln" /build "Release|x86" /project "d3d9"  /out "logs\d3d9.log"
 
-Write-Host "build lua plugin"
-#build plugin
-devenv  "M2TWEOP-luaPlugin\luaPlugin.sln" /build "Release|x86" /project "luaPlugin"  /out "logs\luaPlugin.log" 
+# 2) Build M2TWEOP-LuaPlugin
+Write-Host "======== 2) Build M2TWEOP-LuaPlugin ========\n" -ForegroundColor Yellow
 
-Write-Host "Make documentation"
+devenv  "M2TWEOP-luaPlugin\luaPlugin.sln" /build "Release|x86" /project "luaPlugin"  /out "logs\luaPlugin.log"
+
+# 3) Build Documentation
+Write-Host "======== 3) Build M2TWEOP-Documentation ========\n" -ForegroundColor Yellow
+
 cd "documentationGenerator"
-
-
 &".\generateDocs.ps1"   -Wait -NoNewWindow | Write-Verbose
 
+# 4) Copy built files
+Write-Host "======== 4) Copy all created files ========\n" -ForegroundColor Yellow
+
 Set-Location -Path $currentLoc
-
-Write-Host "Copy all created files"
-
-Remove-item ./M2TWEOPGenerated -recurse -erroraction 'silentlycontinue' 
-new-item ./M2TWEOPGenerated  -itemtype directory -erroraction 'silentlycontinue' 
+Remove-item ./M2TWEOPGenerated -recurse -erroraction 'silentlycontinue'
+new-item ./M2TWEOPGenerated  -itemtype directory -erroraction 'silentlycontinue'
 
 
-Copy-Item -Path  "M2TWEOP-DataFiles\*" -Destination "./M2TWEOPGenerated" -recurse 
-Copy-Item -Path  "documentationGenerator\EOPDocs\build\html\*" -Destination "./M2TWEOPGenerated/eopData/helpPages" -recurse 
+Copy-Item -Path  "M2TWEOP-DataFiles\*" -Destination "./M2TWEOPGenerated" -recurse
+Copy-Item -Path  "documentationGenerator\EOPDocs\build\html\*" -Destination "./M2TWEOPGenerated/eopData/helpPages" -recurse
 
 Copy-Item -Path  "M2TWEOP-luaPlugin\Release\luaPlugin.dll" -Destination "./M2TWEOPGenerated/youneuoy_Data/plugins"
 Copy-Item -Path  "M2TWEOP-library\Release\d3d9.dll" -Destination "./M2TWEOPGenerated"
-Copy-Item -Path  "M2TWEOP-library\Release\M2TWEOP GUI.exe" -Destination "./M2TWEOPGenerated" 
-Copy-Item -Path  "M2TWEOP-library\Release\M2TWEOP tools.exe" -Destination "./M2TWEOPGenerated" 
-Copy-Item -Path  "M2TWEOP-library\Release\M2TWEOPLibrary.dll" -Destination "./M2TWEOPGenerated" 
+Copy-Item -Path  "M2TWEOP-library\Release\M2TWEOP GUI.exe" -Destination "./M2TWEOPGenerated"
+Copy-Item -Path  "M2TWEOP-library\Release\M2TWEOP tools.exe" -Destination "./M2TWEOPGenerated"
+Copy-Item -Path  "M2TWEOP-library\Release\M2TWEOPLibrary.dll" -Destination "./M2TWEOPGenerated"
 
-Remove-item M2TWEOP.zip -erroraction 'silentlycontinue' 
+# 5) Generate Release ZIP
+Write-Host "======== 5) Generate Release ZIP ========\n" -ForegroundColor Yellow
+Remove-item M2TWEOP.zip -erroraction 'silentlycontinue'
 Compress-Archive -Path "./M2TWEOPGenerated/*"  -DestinationPath "M2TWEOP.zip"
-Remove-item ./M2TWEOPGenerated -recurse -erroraction 'silentlycontinue' 
-Write-Host "All done"
+Remove-item ./M2TWEOPGenerated -recurse -erroraction 'silentlycontinue'
+
+
+# 6) Done
+Write-Host "======== 6) Success! EOP Built Successfully! ========\n" -ForegroundColor Yellow
 pause
--- documentationGenerator/generateDocs.ps1
+++ documentationGenerator/generateDocs.ps1
@@ -1,17 +1,23 @@
 #remove old lua doc files
-Remove-item generatedLuaDocs -recurse -ErrorAction Ignore
+Write-Host "======== 3.1) Remove old documentation ========\n" -ForegroundColor Yellow
+Remove-item generatedLuaDocs -recurse  -erroraction 'silentlycontinue'
 
 #create folder for temporary place scripting docs
 new-item ./generatedLuaDocs -itemtype directory
 
+Write-Host "======== 3.2) Generate documentation config ========\n" -ForegroundColor Yellow
 #.\luarocks\ldoc.lua.bat . -c ../luaPlugin/config.ld
 .\luarocks\lua.exe ./luarocks/rocks/ldoc/1.4.6-2/bin/ldoc.lua . -c config.ld
 
+Write-Host "======== 3.3) Remove old files ========\n" -ForegroundColor Yellow
 #remove old scripting files in main docs path
-Remove-item EOPDocs/source/_static/LuaLib/* -recurse
+Remove-item EOPDocs/source/_static/LuaLib/* -recurse  -erroraction 'silentlycontinue'
 #erase build path
-Remove-item EOPDocs/build/* -recurse
+Remove-item EOPDocs/build/* -recurse  -erroraction 'silentlycontinue'
+
+Write-Host "======== 3.4) Build documentation files  ========\n" -ForegroundColor Yellow
 #copy created scripting docs to main docs source folder
 Copy-Item -Path "generatedLuaDocs/*" -Destination "EOPDocs/source/_static/LuaLib" -recurse
-
 Start-Process -FilePath ".\EOPDocs\WPy32-3890\scripts\cmdEOPDOCS.bat" -Wait -NoNewWindow
+
+Write-Host "======== 3.5) Success! Documentation built successfully.  ========\n" -ForegroundColor Yellow
