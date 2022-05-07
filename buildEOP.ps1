a2a65c3216ae56b120aeaf222160d6f9896e0d4c
Eddie Eldridge <steadyeddie101@hotmail.co.uk>
2022-05-07 23:05:36 +0100

    Release: This will work

--- buildEOP.ps1
+++ buildEOP.ps1
@@ -42,7 +42,7 @@ new-item ./M2TWEOPGenerated  -itemtype directory -erroraction 'continue'
 Copy-Item -Path  "M2TWEOP-DataFiles\*" -Destination "./M2TWEOPGenerated" -recurse
 
 Get-ChildItem -Path "documentationGenerator\EOPDocs\build\html\*" -erroraction 'continue'
-xcopy "documentationGenerator\EOPDocs\build\html" "./M2TWEOPGenerated/eopData/helpPages" /l /i /s /y
+xcopy  /E /Y "documentationGenerator\EOPDocs\build\html\*" "./M2TWEOPGenerated/eopData/helpPages"
 
 Copy-Item -Path  "M2TWEOP-luaPlugin\Release\luaPlugin.dll" -Destination "./M2TWEOPGenerated/youneuoy_Data/plugins" -erroraction 'continue'
 Copy-Item -Path  "M2TWEOP-library\Release\d3d9.dll" -Destination "./M2TWEOPGenerated" -erroraction 'continue'
